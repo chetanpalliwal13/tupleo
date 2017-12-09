f230d60d6c645721512023182c1df97bc7f253d9
chetanpalliwal13 <33956272+chetanpalliwal13@users.noreply.github.com>
2017-12-09 15:22:54 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='tupleo',
+      version='1.0',
+      description='A tuple operations API',
+      url='https://github.com/chetanpalliwal13/tupleo',
+      author='chetanpalliwal13',
+      author_email='chetanpalliwal13@gmail.com',
+      license='GPL',
+      packages=['tupleo'],
+      zip_safe=False,
+      extras_require={
+          'magic': ['python-magic']
+      })
