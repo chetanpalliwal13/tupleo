f633bc1fb31a9e7092172ad57c1342d892b3e2ea
chetanpalliwal13 <33956272+chetanpalliwal13@users.noreply.github.com>
2017-12-09 16:14:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tupleo',
-      version='1.00.00',
+      version='1.0.0',
       description='A tuple operations API',
       url='https://github.com/chetanpalliwal13/tupleo',
       author='chetanpalliwal13',
