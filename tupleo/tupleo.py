a9ca0150b0c9f8161b22c152999e23894af9ba96
Chetan Palliwal <33956272+chetanpalliwal13@users.noreply.github.com>
2017-12-10 18:46:57 +0530

    Update tupleo.py

--- tupleo/tupleo.py
+++ tupleo/tupleo.py
@@ -107,3 +107,29 @@ def sort(tupleo, key=None, reverse=False):
     convertlist = list(tupleo)
     convertlist.sort(key=key, reverse=reverse)
     return tuple(convertlist)
+
+
+# Code for conversion of tuple, list of tuple to List and Dictionary.
+# Conversion:
+#    - Formal conversion
+#    - Index based conversion
+
+def tupleToDict(tupleo, index=0):
+    """
+    tupleToDict(...) method of tupleo.tuple instance
+    T.tupleToDict(tupleo, object) -> None -- convert tuple to Dictionary.
+    if index given then index is key and remain elements are value in list format (default index 0)
+    """
+    dict = {}
+    if len(tupleo)==0:
+        return dict
+    elif len(tupleo)==1:
+        dict.update({tupleo[0]:''})
+        return dict
+    else:
+        if index==0:
+            dict.update({tupleo[0]:list(tupleo[1:])})
+            return dict
+        else:
+            dict.update({tupleo[index]:list(tupleo.pop(index))})
+            return dict
